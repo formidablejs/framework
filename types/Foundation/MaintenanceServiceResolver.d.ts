fe8c71b977fe9e22d3edea674660690395d1222b
Donald Pakkies <donaldpakkies@gmail.com>
2021-10-02 10:18:16 +0200

    build framework

--- types/Foundation/MaintenanceServiceResolver.d.ts
+++ types/Foundation/MaintenanceServiceResolver.d.ts
@@ -1,3 +1,22 @@
 export default class MaintenanceServiceResolver extends ServiceResolver {
+    get cookieName(): string;
+    getDown(): false | {
+        message: any;
+        statusCode: any;
+        secret: any;
+        redirect: any;
+        retry: number;
+        refresh: number;
+    };
+    /**
+    @param {String} secret
+    @param {FormRequest} request
+    */
+    hasBypassMaintenanceModeCookie(secret: string, request: FormRequest): boolean;
+    /**
+    @param {FormRequest} request
+    */
+    setBypassMaintenanceModeCookie(secret: any, request: FormRequest): any;
 }
 import ServiceResolver from "../Support/ServiceResolver";
+import FormRequest from "../Http/Request/FormRequest";
