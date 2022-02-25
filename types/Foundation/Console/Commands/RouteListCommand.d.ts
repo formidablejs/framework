ea99b7c1bff980989d6a0e7e29fd8d5fcf165edf
Donald Pakkies <donaldpakkies@gmail.com>
2022-02-25 19:10:27 +0000

    feat: build imba and generate types

---
+++ types/Foundation/Console/Commands/RouteListCommand.d.ts
@@ -0,0 +1,4 @@
+export class RouteListCommand extends Command {
+    get app(): any;
+}
+import { Command } from "@formidablejs/console";
--- types/Foundation/ConsoleKernel.d.ts
+++ types/Foundation/ConsoleKernel.d.ts
@@ -1,12 +1,12 @@
 export default class ConsoleKernel {
-    get default(): (typeof MigrateLatestCommand | typeof GenerateKeyCommand | typeof MakeConfigCommand | typeof UpCommand)[];
+    get default(): (typeof RouteListCommand | typeof GenerateKeyCommand | typeof MakeConfigCommand | typeof UpCommand)[];
     get registered(): any[];
     /**
     @param {Application} app
     */
     registerCommands(app: Application, ctx: any): any[];
 }
-import { MigrateLatestCommand } from "./Console/Commands/MigrateLatestCommand";
+import { RouteListCommand } from "./Console/Commands/RouteListCommand";
 import { GenerateKeyCommand } from "./Console/Commands/GenerateKeyCommand";
 import { MakeConfigCommand } from "./Console/Commands/MakeConfigCommand";
 import { UpCommand } from "./Console/Commands/UpCommand";
