1c6d8af96c1a09ede35ef240d3814852812a72c9
Donald Pakkies <donaldpakkies@gmail.com>
2022-03-31 09:10:37 +0000

    chore: remove supertest from scripts

--- scripts/2-install.sh
+++ scripts/2-install.sh
@@ -22,6 +22,5 @@ echo
 cd $E2E                           && \
 npm i --legacy-peer-deps          && \
 npm i -g pm2                      && \
-npm i supertest                   && \
 npm i @vscode/sqlite3 --save      && \
 npm i $PACKAGE --legacy-peer-deps
