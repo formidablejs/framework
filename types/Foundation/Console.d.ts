d96c1d777e2c3afec744ff537d97d375e5e06f1e
Donald Pakkies <donaldpakkies@gmail.com>
2022-04-02 10:52:12 +0000

    feat: build imba and ts

--- types/Foundation/Console.d.ts
+++ types/Foundation/Console.d.ts
@@ -1,9 +1,13 @@
 /// <reference types="node" />
 export default class Console {
-    static make(): Console;
-    constructor($$?: any);
-    runtime: any;
-    console: any;
+    static make(runtime: any, console: any): Console;
+    /**
+    @param {String} runtime
+    @param {String} console
+    */
+    constructor(runtime: string, console: string);
+    runtime: string;
+    console: string;
     config: any;
     run(): import("child_process").ChildProcessWithoutNullStreams;
     [$2]($$?: {}): void;
