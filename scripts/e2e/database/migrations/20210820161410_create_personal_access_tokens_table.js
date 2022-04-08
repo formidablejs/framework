ec72193690d84653dc34cdee2069feebb597d41b
Donald Pakkies <donaldpakkies@gmail.com>
2022-04-08 16:58:59 +0000

    feat: update personal_access_tokens migrations for tests

--- scripts/e2e/database/migrations/20210820161410_create_personal_access_tokens_table.js
+++ scripts/e2e/database/migrations/20210820161410_create_personal_access_tokens_table.js
@@ -1,15 +1,19 @@
+const { Knex } = require('knex');
+
+/** @param {Knex} knex */
 exports.up = (knex) => {
 	return knex.schema.createTable('personal_access_tokens', (table) => {
 		table.increments('id').primary();
 		table.string('tokenable_type');
-		table.integer('tokenable_id');
+		table.bigInteger('tokenable_id').index().unsigned();
 		table.string('name');
 		table.string('abilities').nullable();
 		table.string('payload').nullable();
-		table.integer('ttl').nullable();
+		table.integer('ttl').index().nullable();
 		table.timestamp('last_used_at').nullable();
 		table.timestamps(true, true);
 	});
 }
 
+/** @param {Knex} knex */
 exports.down = (knex) => knex.schema.dropTable('personal_access_tokens');
