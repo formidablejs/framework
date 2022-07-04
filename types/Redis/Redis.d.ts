9772ba4e924cefb36a3965690428e91e49ca107f
Donald Pakkies <donaldpakkies@gmail.com>
2022-07-04 19:57:43 +0200

    feat: build imba and generate types

--- types/Redis/Redis.d.ts
+++ types/Redis/Redis.d.ts
@@ -1,9 +1,8 @@
 export default class Redis {
     /**
     @param {String} database
-    @param {Object} config
     */
-    static connection(database?: string, config?: any): Promise<any>;
+    static connection(database?: string): Promise<any>;
     static configure(config: any): any;
     static closeAll(): any[];
     /**
@@ -29,7 +28,6 @@ export default class Redis {
     static command(command: string, key: string, value?: string | null, nx?: any): Promise<any>;
     /**
     @param {String} database
-    @param {Object} config
     */
-    constructor(database?: string, config?: any);
+    constructor(database?: string);
 }
