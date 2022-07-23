df2a9f825e695ab196e09fde6a9b65ee0a288b76
Donald Pakkies <donaldpakkies@gmail.com>
2022-07-23 18:47:53 +0200

    feat: build imba and generate types

--- types/Foundation/Console/Commands/MakeResourceCommand.d.ts
+++ types/Foundation/Console/Commands/MakeResourceCommand.d.ts
@@ -1,5 +1,7 @@
 export class MakeResourceCommand extends Command {
     get resource(): string;
+    get package(): string;
+    get language(): any;
     get stub(): import("@formidablejs/stubs/types/Stub");
     handle(): void;
 }
