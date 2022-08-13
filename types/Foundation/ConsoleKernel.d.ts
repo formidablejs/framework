d4edf57f32f20a96720d0c210e02679772bbfc58
Donald Pakkies <donaldpakkies@gmail.com>
2022-08-13 19:18:30 +0200

    feat: build imba and ts

--- types/Foundation/ConsoleKernel.d.ts
+++ types/Foundation/ConsoleKernel.d.ts
@@ -4,7 +4,11 @@ export default class ConsoleKernel {
     /**
     @param {Application} app
     */
-    registerCommands(app: Application, ctx: any): any[];
+    registerCommands(app: Application, ctx: any): any[][];
+    /**
+    @param {Application} app
+    */
+    loadEvents(app: Application): any[][];
 }
 import { EnvironmentCommand } from "./Console/Commands/EnvironmentCommand";
 import { RouteListCommand } from "./Console/Commands/RouteListCommand";
