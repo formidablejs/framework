736f197b8c3e7290277d02fab31913b09977bfcf
Donald Pakkies <donaldpakkies@gmail.com>
2022-09-05 17:42:51 +0200

    feat: build imba and ts

--- types/Foundation/Console/Commands/ServeCommand.d.ts
+++ types/Foundation/Console/Commands/ServeCommand.d.ts
@@ -1,5 +1,6 @@
-/// <reference types="node" />
 export class ServeCommand extends Command {
+    static [$__init__$](): typeof ServeCommand;
+    constructor(...args: any[]);
     get props(): {
         port: any;
         host: any;
@@ -9,7 +10,17 @@ export class ServeCommand extends Command {
     get runtime(): string;
     get fallbackPort(): string;
     get fallbackHost(): string;
-    handle(): void | (import("child_process").SpawnSyncReturns<Buffer> & import("child_process").SpawnSyncReturns<string> & import("child_process").SpawnSyncReturns<string | Buffer>);
     setEnvVars(): mixed;
+    [$__patch__$]($$?: {}): void;
+    [$__init__$]($$?: any, deep?: boolean, ...args: any[]): void;
+    [$address$]: any;
+    [$fullAddress$]: any;
+    [$command$]: any;
 }
 import { Command } from "../Command";
+declare const $__patch__$: unique symbol;
+declare const $__init__$: unique symbol;
+declare const $address$: unique symbol;
+declare const $fullAddress$: unique symbol;
+declare const $command$: unique symbol;
+export {};
