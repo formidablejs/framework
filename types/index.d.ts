213f6a0eaa0ce4ca4ab02c4dea8282705863e3d7
Donald Pakkies <donaldpakkies@gmail.com>
2024-08-13 20:57:22 +0200

    chore: update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -27,6 +27,7 @@ import Auth from "./Auth/Auth";
 import Authenticate from "./Auth/Http/Middleware/Authenticate";
 import AuthenticationServiceResolver from "./Auth/AuthenticationServiceResolver";
 import AuthorizationException from "./Auth/Exceptions/AuthorizationException";
+import Authorize from "./Auth/Authorize";
 import AuthService from "./Auth/AuthService";
 import config from "./Support/Helpers/config";
 import ConfigRepository from "./Config/Repository";
@@ -175,6 +176,7 @@ export {
     Authenticate,
     AuthenticationServiceResolver,
     AuthorizationException,
+    Authorize,
     AuthService,
     Command,
     config,
