dac83e446e32e9d0c2f4e0427dd85535fe700049
Donald Pakkies <donaldpakkies@gmail.com>
2025-02-20 21:52:03 +0200

    fix: code

--- types/Mail/Mailable.d.ts
+++ types/Mail/Mailable.d.ts
@@ -1,7 +1,6 @@
 import { Mailable as MailableBase } from '@formidablejs/mailer'
 import { config as ConfigFinder } from '../Support/Helpers/index'
 
-export class Mailable < MailableBase
-
-	get config
-		ConfigFinder
+export class Mailable extends MailableBase {
+  config: typeof ConfigFinder
+}
