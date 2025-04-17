97849408f6042c68540b159d1e34ea6c606bceb2
Donald Pakkies <donaldpakkies@gmail.com>
2025-04-17 14:47:53 +0200

    chore: undo changes

--- scripts/e2e/database/migrations/20211126141343_create_posts_table.js
+++ scripts/e2e/database/migrations/20211126141343_create_posts_table.js
@@ -1,20 +1,9 @@
-const { Knex } = require('knex');
-
-/** @param {Knex} knex */
 exports.up = (knex) => {
-  knex.schema.hasTable('posts')
-    .then((exists) => {
-      if (!exists) {
-        return knex.schema.createTable('posts', (table) => {
-          table.increments('id').primary();
-          table.string('body');
-          table.timestamps(true, true);
-        });
-      }
-    })
-    .catch((error) => {
-      console.error('Error checking for posts table:', error);
-    });
+	return knex.schema.createTable('posts', (table) => {
+		table.increments('id').primary();
+		table.string('body');
+		table.timestamps(true, true);
+	});
 };
 
 exports.down = (knex) => knex.schema.dropTable('posts');
