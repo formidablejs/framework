812badd852029af69833a1052dc301b493bd106b
Donald Pakkies <donaldpakkies@gmail.com>
2025-04-17 14:43:48 +0200

    fix: check if tables already exists or not before attempting to create them

--- scripts/e2e/database/migrations/20211126141343_create_posts_table.js
+++ scripts/e2e/database/migrations/20211126141343_create_posts_table.js
@@ -1,9 +1,20 @@
+const { Knex } = require('knex');
+
+/** @param {Knex} knex */
 exports.up = (knex) => {
-	return knex.schema.createTable('posts', (table) => {
-		table.increments('id').primary();
-		table.string('body');
-		table.timestamps(true, true);
-	});
+  knex.schema.hasTable('posts')
+    .then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('posts', (table) => {
+          table.increments('id').primary();
+          table.string('body');
+          table.timestamps(true, true);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error('Error checking for posts table:', error);
+    });
 };
 
 exports.down = (knex) => knex.schema.dropTable('posts');
