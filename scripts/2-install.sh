78183e888c43e731687a01e567b360608bacd864
Donald Pakkies <donaldpakkies@gmail.com>
2025-04-21 20:38:16 +0200

    feat: add support for bun package manager

--- scripts/2-install.sh
+++ scripts/2-install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+RUNTIME=${RUNTIME:-node}
+PKG_MGR="npm"
+
+if [ "$RUNTIME" = "bun" ]; then
+    PKG_MGR="bun"
+fi
+
 PACKAGE=${PWD}/output/package.tgz
 E2E=${PWD}/test/e2e
 
@@ -19,7 +26,14 @@ echo
 echo "Install dependencies..."
 echo
 
-cd $E2E                           && \
-npm i --legacy-peer-deps          && \
-npm i sqlite3 --save              && \
-npm i $PACKAGE --legacy-peer-deps
+cd "$E2E" || exit 1
+
+if [ "$PKG_MGR" = "bun" ]; then
+    bun install        && \
+    bun add sqlite3    && \
+    bun add "$PACKAGE"
+else
+    npm install --legacy-peer-deps            && \
+    npm install sqlite3 --save                && \
+    npm install "$PACKAGE" --legacy-peer-deps
+fi
