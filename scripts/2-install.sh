c7e59db6f0f9d954b0a881f3fdb4baa95b6ebcc9
Donald Pakkies <donaldpakkies@gmail.com>
2025-04-21 21:08:53 +0200

    chore: cat package.json

--- scripts/2-install.sh
+++ scripts/2-install.sh
@@ -31,6 +31,7 @@ cd "$E2E" || exit 1
 if [ "$PKG_MGR" = "bun" ]; then
     rm -rf node_modules yarn.lock package-lock.json bun.lockb && \
     sed -i '/"@formidablejs\/framework"/d' package.json       && \
+    cat package.json                                          && \
     bun add sqlite3 "$PACKAGE"
 else
     npm install --legacy-peer-deps            && \
